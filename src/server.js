c8ca0c2f338785551f22ec142565251e93d4ea5d
Vinicius <v.nascimento23@yahoo.com.br>
2023-01-12 15:20:13 -0300

    feat: add initial structuring and styling for the blog page

--- src/server.js
+++ src/server.js
@@ -11,10 +11,15 @@ const port = 3000;
 app.get("/", (req, res) => {
     const articles = [{
         title: "Artigo teste",
-        createdAt: Date.now(),
+        createdAt: new Date,
         description: "Descrição teste "
-    }]
-    res.render("index", { articles: articles })
+    },
+    {
+        title: "Artigo teste",
+        createdAt: new Date,
+        description: "Descrição teste "
+    }];
+    res.render("index", { articles: articles });
 });
 
 app.listen(port, () => console.log(`O app está rodando na porta ${port}`));
